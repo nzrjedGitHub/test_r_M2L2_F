5b62d2c700a40114fd4dbe2815560b25632e21c9
nzrjedGitHub <98401179+nzrjedGitHub@users.noreply.github.com>
2024-10-23 19:11:23 +0300

    complete prj

--- main_app.py
+++ main_app.py
@@ -1,5 +1,3 @@
-# напиши тут свою програму
-# напиши тут свою програму
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.boxlayout import BoxLayout
@@ -9,8 +7,10 @@ from kivy.uix.textinput import TextInput
 from kivy.core.window import Window
 from kivy.uix.scrollview import ScrollView
 from instructions import txt_instruction, txt_test1, txt_test2, txt_test3, txt_sits
+from runner import Runner
 from seconds import Seconds
-
+from sits import Sits
+from ruffier import test
 
 age = 7
 name = ""
@@ -114,66 +114,132 @@ class PulseScr(Screen):
 class CheckSits(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+
+        self.next_screen = False
+
         instr = Label(text=txt_sits)
+        self.lbl_sits = Sits(3)
+        self.run = Runner(total=3, steptime=1.5, size_hint=(0.4, 1))
+        self.run.bind(finished=self.run_finished)
+
+        line = BoxLayout()
+        vlay = BoxLayout(orientation='vertical', size_hint=(0.3, 1))
+        vlay.add_widget(self.lbl_sits)
+        line.add_widget(instr)
+        line.add_widget(vlay)
+        line.add_widget(self.run)
+
         self.btn = Button(
             text="Продовжити", size_hint=(0.3, 0.2), pos_hint={"center_x": 0.5}
         )
         self.btn.on_press = self.next
         outer = BoxLayout(orientation="vertical", padding=8, spacing=8)
-        outer.add_widget(instr)
+        outer.add_widget(line)
         outer.add_widget(self.btn)
         self.add_widget(outer)
 
     def next(self):
-        self.manager.current = "pulse2"
+        if self.next_screen == False:
+           self.btn.set_disabled(True)
+           self.run.start()
+           self.run.bind(value=self.lbl_sits.next)
+        else:
+           self.manager.current = 'pulse2'
+           
+    def run_finished(self, instance, value):
+       self.btn.set_disabled(False)
+       self.btn.text = 'Продовжити'
+       self.next_screen = True
+    
 
 
 class PulseScr2(Screen):
     def __init__(self, **kwargs,):
         super().__init__( **kwargs)
+
+        self.next_screen = False
+        self.stage = 0
+
         instr = Label(text=txt_test3)
         line1 = BoxLayout(size_hint=(0.8, None), height="30sp")
+
+        self.lbl_sec = Seconds(3)
+        self.lbl_sec.bind(done=self.sec_finished)
+        self.lbl1 = Label(text="Заміряйте пульс")
+
         lbl_result1 = Label(text="Результат:", halign="right")
         self.in_result1 = TextInput(text="0", multiline=False)
+
         line1.add_widget(lbl_result1)
         line1.add_widget(self.in_result1)
+
         line2 = BoxLayout(size_hint=(0.8, None), height="30sp")
         lbl_result2 = Label(text="Результат після відпочинку:", halign="right")
         self.in_result2 = TextInput(text="0", multiline=False)
+
         line2.add_widget(lbl_result2)
         line2.add_widget(self.in_result2)
+
+        self.in_result1.set_disabled(True)#
+        self.in_result2.set_disabled(True)#
+
         self.btn = Button(
-            text="Завершити", size_hint=(0.3, 0.2), pos_hint={"center_x": 0.5}
+            text="Почати", size_hint=(0.3, 0.2), pos_hint={"center_x": 0.5}
         )
         self.btn.on_press = self.next
         outer = BoxLayout(orientation="vertical", padding=8, spacing=8)
-        outer.add_widget(instr)
+        outer.add_widget(instr) 
+        outer.add_widget(self.lbl1) #
+        outer.add_widget(self.lbl_sec) #
         outer.add_widget(line1)
         outer.add_widget(line2)
         outer.add_widget(self.btn)
         self.add_widget(outer)
 
+    def sec_finished(self, *args):
+        if self.lbl_sec.done:
+            if self.stage == 0:
+                self.stage = 1
+                self.lbl1.text = "Відпочивайте"
+                self.lbl_sec.restart(3)
+                self.in_result1.set_disabled(False)
+            elif self.stage == 1:
+                self.stage = 2
+                self.lbl1.text = "Заміряйте пульс"
+                self.lbl_sec.restart(3)
+            elif self.stage == 2:
+                self.in_result2.set_disabled(False)
+                self.btn.set_disabled(False)
+                self.btn.text = "Завершити"
+                self.next_screen = True
     def next(self):
-        global p2, p3
-        p2 = check_int(self.in_result1.text)
-        p3 = check_int(self.in_result2.text)
-        if p2 == False:
-            p2 = 0
-            self.in_result1.text = str(p2)
-        elif p3 == False:
-            p3 = 0
-            self.in_result2.text = str(p3)
+        if not self.next_screen:
+            self.btn.set_disabled(True)
+            self.lbl_sec.start()
         else:
-            self.manager.current = "result"
-
+            global p2, p3
+            p2 = check_int(self.in_result1.text)
+            p3 = check_int(self.in_result2.text)
+            if p2 == False:
+                p2 = 0
+                self.in_result1.text = str(p2)
+            elif p3 == False:
+                p3 = 0
+                self.in_result2.text = str(p3)
+            else:
+                self.manager.current = "result"
 
 class Result(Screen):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.outer = BoxLayout(orientation="vertical", padding=8, spacing=8)
-        self.instr = Label(text="place for results")
-        self.outer.add_widget(self.instr)
-        self.add_widget(self.outer)
+   def __init__(self, **kwargs):
+      super().__init__(**kwargs)
+      self.outer = BoxLayout(orientation='vertical', padding=8, spacing=8)
+      self.instr = Label(text = '')
+      self.outer.add_widget(self.instr)
+      self.add_widget(self.outer)
+      self.on_enter = self.before
+   def before(self):
+      global name
+      self.instr.text = name + '\n' + test(p1, p2, p3, age)
 
 
 class HeartCheck(App):
--- sits.py
+++ sits.py
@@ -1,9 +1,16 @@
 # напиши модуль для підрахунку кількості присідань
+from kivy.uix.label import Label
 
 class Sits(Label):
-    
-    def __init__(self, total, **kwargs):
-        pass
+   def __init__(self, total, **kwargs):
+       self.current = 0
+       self.total = total
+       my_text = "Залишилось присідань: " + str(self.total)
+       super().__init__(text=my_text, **kwargs)
 
-    def next(self, *args):
-        pass
+
+   def next(self, *args):
+       self.current += 1
+       remain = max(0, self.total - self.current)
+       my_text = "Залишилось присідань: " + str(remain)
+       self.text=my_text
